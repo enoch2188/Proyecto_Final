19f68b239151d845e1694103de248672e3cf1b4a
enoch2188 <sebaperotti@hotmail.com>
2023-06-06 15:18:29 -0300

    empiezo javascript

---
+++ index.js
@@ -0,0 +1,85 @@
+console.log('Soy el mejor')
+
+
+// palabras reservadas -- Sintaxis
+// var - const - break - instanceof - typeof - if - case 
+// catch - continue - default - let - delete - do - else - finally
+//  for  - functoin - if - in - inew - return - switch - this
+//  throw - try - void - while - with - etc etc etc
+
+// Variables y Valores - caja para guardar tipo de datos (string)
+// para poder crear una caja yo tengo que utilizar ciertas palabras reservadas
+// por ejemplo...
+// var   nombre   = 'Sebastian'
+// var es para crear la caja, = es para guardar el valor que yo quiera
+// en este caso, un string.
+// let   apellido = 'Perotti'
+// const CURSO    = "Javascript"
+// const curso    = 'No es java'
+
+// Declaracion
+// let apellido
+// var nombre
+// const curso
+
+// var nombre_apellido
+// var nombreApellidoAnio  // camel case    
+// var NombreApellidoAnio   // pascal case
+// // var nombre y apelllido (incorrecto)
+// var anio
+
+// Asignacion
+// let edad = 30
+// Inicializando variables
+// const edad1 = 30
+
+// Reasignar
+// edad = 35
+// edad1 = 40
+
+// no usemos variables creadas con var
+// porque no
+
+//  let numero    = 1
+//  let numero2   = 2
+//  const NUMERO3 = 3
+
+//  let resultado      = numero + numero2 // es 1+2
+//  let resultadoResta = numero - numero2 // es 1-2
+//  let multiplicacion = numero * NUMERO3 // es 1*3
+
+
+// //  operaciones basicas con string
+// let texto1   = 'Seba'
+// let texto2   = 'Perotti'
+// const texto3 = ' '
+
+// let resultadoSuma = texto1 + texto2
+// let resultadoSuma1 = texto1 + 1
+// let resultadoSuma2 = texto1 + texto3 + texto2
+
+// console.log(resultadoSuma2)
+
+// prompt
+// let nombreIngresadoPrompt = prompt('ingresar nombre, apura')
+// console.log(nombreIngresadoPrompt)
+
+// alert
+// alert('Bienvenido a esta pagina')
+
+// ejemplo
+// let entrada = prompt('Ingrese un Nombre: ')
+// let salida  = 'El nombre ingresasdo es: ' + entrada
+// console.log(salida)
+// alert(salida)
+
+
+let entrada = prompt('Quien fue el mejor numero 10?: ')
+console.log(typeof entrada)
+
+let salida  = 'El nombre ingresado es: ' + entrada
+
+console.log(salida)
+
+// alert(salida)
+
