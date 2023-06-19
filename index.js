b166cf49f519d617a3935b455fb7c93c547a0083
enoch2188 <sebaperotti@hotmail.com>
2023-06-19 18:00:14 -0300

    cierro esta branch para crear una nueva para la primera preentrega de javascript

--- index.js
+++ index.js
@@ -1,85 +1,127 @@
-console.log('Soy el mejor')
+// console.log('Soy el mejor')
 
 
-// palabras reservadas -- Sintaxis
-// var - const - break - instanceof - typeof - if - case 
-// catch - continue - default - let - delete - do - else - finally
-//  for  - functoin - if - in - inew - return - switch - this
-//  throw - try - void - while - with - etc etc etc
+// // palabras reservadas -- Sintaxis
+// // var - const - break - instanceof - typeof - if - case 
+// // catch - continue - default - let - delete - do - else - finally
+// //  for  - functoin - if - in - inew - return - switch - this
+// //  throw - try - void - while - with - etc etc etc
 
-// Variables y Valores - caja para guardar tipo de datos (string)
-// para poder crear una caja yo tengo que utilizar ciertas palabras reservadas
-// por ejemplo...
-// var   nombre   = 'Sebastian'
-// var es para crear la caja, = es para guardar el valor que yo quiera
-// en este caso, un string.
-// let   apellido = 'Perotti'
-// const CURSO    = "Javascript"
-// const curso    = 'No es java'
+// // Variables y Valores - caja para guardar tipo de datos (string)
+// // para poder crear una caja yo tengo que utilizar ciertas palabras reservadas
+// // por ejemplo...
+// // var   nombre   = 'Sebastian'
+// // var es para crear la caja, = es para guardar el valor que yo quiera
+// // en este caso, un string.
+// // let   apellido = 'Perotti'
+// // const CURSO    = "Javascript"
+// // const curso    = 'No es java'
 
-// Declaracion
-// let apellido
-// var nombre
-// const curso
+// // Declaracion
+// // let apellido
+// // var nombre
+// // const curso
 
-// var nombre_apellido
-// var nombreApellidoAnio  // camel case    
-// var NombreApellidoAnio   // pascal case
-// // var nombre y apelllido (incorrecto)
-// var anio
+// // var nombre_apellido
+// // var nombreApellidoAnio  // camel case    
+// // var NombreApellidoAnio   // pascal case
+// // // var nombre y apelllido (incorrecto)
+// // var anio
 
-// Asignacion
-// let edad = 30
-// Inicializando variables
-// const edad1 = 30
+// // Asignacion
+// // let edad = 30
+// // Inicializando variables
+// // const edad1 = 30
 
-// Reasignar
-// edad = 35
-// edad1 = 40
+// // Reasignar
+// // edad = 35
+// // edad1 = 40
 
-// no usemos variables creadas con var
-// porque no
+// // no usemos variables creadas con var
+// // porque no
 
-//  let numero    = 1
-//  let numero2   = 2
-//  const NUMERO3 = 3
+// //  let numero    = 1
+// //  let numero2   = 2
+// //  const NUMERO3 = 3
 
-//  let resultado      = numero + numero2 // es 1+2
-//  let resultadoResta = numero - numero2 // es 1-2
-//  let multiplicacion = numero * NUMERO3 // es 1*3
+// //  let resultado      = numero + numero2 // es 1+2
+// //  let resultadoResta = numero - numero2 // es 1-2
+// //  let multiplicacion = numero * NUMERO3 // es 1*3
 
 
-// //  operaciones basicas con string
-// let texto1   = 'Seba'
-// let texto2   = 'Perotti'
-// const texto3 = ' '
+// // //  operaciones basicas con string
+// // let texto1   = 'Seba'
+// // let texto2   = 'Perotti'
+// // const texto3 = ' '
 
-// let resultadoSuma = texto1 + texto2
-// let resultadoSuma1 = texto1 + 1
-// let resultadoSuma2 = texto1 + texto3 + texto2
+// // let resultadoSuma = texto1 + texto2
+// // let resultadoSuma1 = texto1 + 1
+// // let resultadoSuma2 = texto1 + texto3 + texto2
 
-// console.log(resultadoSuma2)
+// // console.log(resultadoSuma2)
 
-// prompt
-// let nombreIngresadoPrompt = prompt('ingresar nombre, apura')
-// console.log(nombreIngresadoPrompt)
+// // prompt
+// // let nombreIngresadoPrompt = prompt('ingresar nombre, apura')
+// // console.log(nombreIngresadoPrompt)
 
-// alert
-// alert('Bienvenido a esta pagina')
+// // alert
+// // alert('Bienvenido a esta pagina')
 
-// ejemplo
-// let entrada = prompt('Ingrese un Nombre: ')
-// let salida  = 'El nombre ingresasdo es: ' + entrada
+// // ejemplo
+// // let entrada = prompt('Ingrese un Nombre: ')
+// // let salida  = 'El nombre ingresasdo es: ' + entrada
+// // console.log(salida)
+// // alert(salida)
+
+
+// let entrada = prompt('Quien fue el mejor numero 10?: ')
+// console.log(typeof entrada)
+
+// let salida  = 'El nombre ingresado es: ' + entrada
+
 // console.log(salida)
-// alert(salida)
+
+// // alert(salida)
 
 
-let entrada = prompt('Quien fue el mejor numero 10?: ')
-console.log(typeof entrada)
 
-let salida  = 'El nombre ingresado es: ' + entrada
 
-console.log(salida)
 
-// alert(salida)
+
+// COMIENZO SEGUNDA CLASE 
+
+// var 
+// const
+// let
+// sirven para crear variables, para declarar variables, var no se usa mas porque se mpuede modificar
+
+// const
+let numero =3
+
+console.log ('Hola Comision 54890 1')
+console.log ('Hola Comision 54890 2')
+
+// if estructura de control para evaluar una condicion
+// if(condicion) {Bloque}
+if(false) {
+    console.log ('Hola Comision 54890 3')
+    console.log ('Hola Comision 54890 4')
+}
+
+// const nombre = 'Seba'
+// const edad = 30
+// const variableCondicion = false //
+
+// if(variableCondicion){ // dato harcodeado ->
+// console.log(typeof variableConst)
+// }
+
+
+
+
+
+
+
+
+
 
